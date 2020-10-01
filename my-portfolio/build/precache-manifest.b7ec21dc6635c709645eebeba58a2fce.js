7b38be3c84ccd4c5de80d17f56e67e865d389146
Shaleem Malik <shaleemmalik6@gmail.com>
2020-10-01 00:26:55 -0400

    ignore build from gitignore

---
+++ my-portfolio/build/precache-manifest.b7ec21dc6635c709645eebeba58a2fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60fb50dc7c8bc2731751c29632fe63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9ecc6fe0d40aaafb65",
+    "url": "/static/css/main.9074baec.chunk.css"
+  },
+  {
+    "revision": "40bf18b32b7c15598e28",
+    "url": "/static/js/2.56a7d759.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.56a7d759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9ecc6fe0d40aaafb65",
+    "url": "/static/js/main.bf338f23.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
